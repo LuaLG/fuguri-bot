48c3dfadcdf17d6c40d8d8b6f4b641384b1d95ce
KunoichiZ <kotonesoul@yahoo.com>
2018-05-03 02:38:57 -0500

    Add an alias for Kai's custom command

--- commands/custom/kai.js
+++ commands/custom/kai.js
@@ -5,6 +5,7 @@ module.exports = class KaiCommand extends Command {
     constructor(client) {
         super(client, {
             name: 'kai',
+            aliases: ['thunder'],
             group: 'custom',
             memberName: 'kai',
             description: 'Sends an image that says to not bulli Kai',
@@ -13,6 +14,7 @@ module.exports = class KaiCommand extends Command {
     }
 
     run(msg) {
+        // if (msg.guild.id === 373826006651240450) else "You can't use this command here"
         msg.channel.send({
             files: [
                 "./images/remu/dontbulli.jpg"
